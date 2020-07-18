927643f487df7ad435e55dfcc8deac92388ceb0c
Amit Botzer <amitbotzer92@gmail.com>
2020-07-18 21:31:04 +0300

    Added the controllers dir and the DALs dir. add the interface for games_data_accessor

---
+++ DAL/game_data_accessor/games_data_accessor.py
@@ -0,0 +1,19 @@
+
+from models.game import Game
+from models.playground import Playground
+from datetime import date
+
+
+class GamedDataAccessor:
+
+    def store_game(self, game: Game):
+        pass
+
+    def get_games(self, play_date: date, time_range: (str, str), playground: Playground = None):
+        pass
+
+    def update_game(self, game: Game):
+        pass
+
+    def delete_game(self, game: Game):
+        pass
---
+++ controllers/game_controller.py
@@ -0,0 +1,5 @@
+
+class GameController():
+
+    def __init__(self, games_data_accessor):
+        self.games_data_accessor = games_data_accessor
